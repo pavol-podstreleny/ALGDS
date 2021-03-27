4df5260335acc31a6e170b986561283b681b52e8
Palo Podstreleny <palo.podstreleny@gmail.com>
2021-03-27 21:00:41 +0800

    Added dynamic programming solution

--- ae_max_subset_sum_no_adjacent.py
+++ ae_max_subset_sum_no_adjacent.py
@@ -11,3 +11,21 @@ def calculateMaxSubset(array, i):
     rightSum = calculateMaxSubset(array, i+3)
     maxSum = max(leftSum, rightSum)
     return array[i] + maxSum
+
+
+def maxSubsetSumNoAdjacentDynamic(array):
+    if len(array) == 0:
+        return 0
+
+    if len(array) == 1:
+        return array[0]
+
+    first = array[0]
+    second = max(first, array[1])
+
+    for i in range(2, len(array)):
+        current = max(second, first+array[i])
+        first = second
+        second = current
+
+    return second
